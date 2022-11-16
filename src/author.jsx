00d4b15aa565801bee4cb52cb6239fb364324ce2
Pélé Oussoumanou <43408405+othimar@users.noreply.github.com>
2022-11-16 18:22:11 +0100

    Update author.jsx

--- src/author.jsx
+++ src/author.jsx
@@ -3,7 +3,7 @@ import './author.css'
 export function Author(){
     return (
         <div className="author">
-            <img src="/public/images/image-jeremy.png" alt='profile picture'/>
+            <img src="/images/image-jeremy.png" alt='profile picture'/>
             <div className="infos">
                 <div class="owner">Report for</div>
                 <h1 className="name">Jeremy Robson</h1>
